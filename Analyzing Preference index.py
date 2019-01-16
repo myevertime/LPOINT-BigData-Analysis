b0da16bde14cde8dab836dce21ae4e7746b0dd61
Hyeonjae Cho <39979050+myevertime@users.noreply.github.com>
2019-01-16 16:34:07 +0900

    Update and rename 선호지수 해석.py to Analyzing Preference index.py

--- Analyzing Preference index.py
+++ Analyzing Preference index.py
@@ -1,136 +1,42 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Coded by another team mem
 
-# In[1]:
-
-
+# Load data
 import pandas as pd
 
-
-# In[2]:
-
-
 data = pd.read_csv("prefer_index.csv")
-
-
-# In[3]:
-
-
 del data['Unnamed: 0']
 
-
-# In[4]:
-
-
-data
-
-
-# In[5]:
-
+# Summarization of data
 
 summarize_data=data.describe()
 
-
-# In[6]:
-
-
+# mean
 summarize_data_mead = summarize_data.loc['mean']
-
-
-# In[7]:
-
 
 summarize_data_mead_df=pd.DataFrame(summarize_data_mead)
 
-
-# In[8]:
-
-
 summarize_data_mead_df
 
-
-# In[9]:
-
-
+# load data of session date, session seq...
 clnt_data = pd.read_csv("04Custom.csv")
-
-
-# In[10]:
-
-
-clnt_data
-
-
-# In[11]:
-
 
 data_merge=pd.merge(data, clnt_data, on="CLNT_ID",how="left")
 
-
-# In[12]:
-
-
-data_merge
-
-
-# In[13]:
-
-
+# remove outliers
 data_merge_1=data_merge[data_merge['가구']>=5.887168e-01]
-
-
-# In[14]:
-
-
-data_merge_1
-
-
-# In[15]:
-
 
 aa=data_merge_1['CLNT_GENDER'].describe()
 
-
-# In[16]:
-
-
 data_merge_1['CLNT_AGE'].describe()
 
-
-# In[17]:
-
-
-aa
-
-
-# In[18]:
-
-
+# make a column list
 col=data.columns
-
-
-# In[19]:
-
 
 col_list = []
 for factor in range(1,38):
     col_list.append(col[factor])
 
-
-# In[20]:
-
-
-col_list
-
-
-# In[21]:
-
-
 summarize_data_mead_df.loc['가구'][0]
-
-
-# In[22]:
-
 
 clac1_clnt_prop_data={"gen_1":[],"gen_1_count":[],"gen_total":[],"age_mean":[]}
 for col in col_list:
@@ -143,75 +49,20 @@ for col in col_list:
     clac1_clnt_prop_data["gen_total"].append(gender[0])
     clac1_clnt_prop_data['age_mean'].append(age[1])
 
-
-# In[23]:
-
-
 clac1_clnt_prop_data_df = pd.DataFrame(clac1_clnt_prop_data)
-
-
-# In[24]:
-
 
 clac1_clnt_prop_data_df['CLAC1']=col_list
 
-
-# In[25]:
-
-
-clac1_clnt_prop_data_df
-
-
-# In[26]:
-
-
 ddata=clac1_clnt_prop_data_df[['CLAC1','gen_1','gen_1_count','gen_total','age_mean']]
 
-
-# In[27]:
-
-
-ddata
-
-
-# In[65]:
-
-
-ddata.to_csv("iiiiiiiiiii.csv")
-
-
-# In[28]:
-
-
-summarize_data
-
-
-# In[ ]:
+ddata.to_csv("result.csv")
 
 
 
-
-
-# In[29]:
-
+# # Data above 75%
 
 summarize_data_mead = summarize_data.loc['75%']
-
-
-# In[30]:
-
-
-data_merge
-
-
-# In[31]:
-
-
 summarize_data_mead_df=pd.DataFrame(summarize_data_mead)
-
-
-# In[32]:
-
 
 clac1_clnt_prop_data={"gen_1":[],"gen_1_count":[],"gen_total":[],"age_75up":[]}
 for col in col_list:
@@ -224,108 +75,36 @@ for col in col_list:
     clac1_clnt_prop_data["gen_total"].append(gender[0])
     clac1_clnt_prop_data['age_75up'].append(age[1])
 
-
-# In[33]:
-
-
 clac1_clnt_prop_data_df = pd.DataFrame(clac1_clnt_prop_data)
-
-
-# In[34]:
-
 
 clac1_clnt_prop_data_df['CLAC1']=col_list
 
-
-# In[35]:
-
-
 ddata=clac1_clnt_prop_data_df[['CLAC1','gen_1','gen_1_count','gen_total','age_75up']]
 
-
-# In[36]:
-
-
-ddata
+ddata.to_csv("result_above_75.csv")
 
 
-# In[85]:
-
-
-ddata.to_csv("ttttttttttt.csv")
-
-
-# In[37]:
-
-
-summarize_data
-
-
-# In[38]:
-
+# # Result btw 50% and 75%
 
 summarize_data_50 = summarize_data.loc['50%']
 summarize_data_75 = summarize_data.loc['75%']
 
-
-# In[39]:
-
-
 summarize_data_50_df=pd.DataFrame(summarize_data_50)
 summarize_data_75_df=pd.DataFrame(summarize_data_75)
 
-
-# In[40]:
-
-
-summarize_data_75_df
-
-
-# In[41]:
-
-
-col_list
-
-
-# In[50]:
-
-
 ddddata = pd.read_csv("Final_2.csv")
-
-
-# In[43]:
-
-
 del ddddata['Unnamed: 0']
-ddddata.head()
-
-
-# In[52]:
-
 
 ddddata_need = ddddata[['CLNT_ID','CLAC1_NM','HITS_SEQ','PD_BUY_CT','SESS_SEQ','TOT_PAG_VIEW_CT','TOT_SESS_HR_V','SESS_DT']]
-
-
-# In[53]:
-
-
-ddddata_need.head()
-
-
-# In[45]:
-
-
 colcol_list=['HITS_SEQ','SESS_SEQ','TOT_PAG_VIEW_CT','TOT_SESS_HR_V']
 
 
-# In[46]:
+
+# # Visualization
 
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-# In[130]:
 
 
 df = pd.DataFrame()
@@ -348,26 +127,9 @@ for col in col_list:
         plt.savefig('C:\\Users\\user\\Desktop\\aaa\\'+i_str+'_'+colcol)
         i+=1
 
-
-# In[47]:
-
-
 dfdf = pd.DataFrame()
-
-
-# In[48]:
-
-
 dfdf['CLAC1']=col_list
-
-
-# In[128]:
-
-
 dfdf.to_csv("index_clac1.csv")
-
-
-# In[131]:
 
 
 df = pd.DataFrame()
@@ -391,9 +153,6 @@ for col in col_list:
         i+=1
 
 
-# In[132]:
-
-
 df = pd.DataFrame()
 i=0
 for col in col_list:
@@ -414,10 +173,6 @@ for col in col_list:
         plt.savefig('C:\\Users\\user\\Desktop\\ccc\\'+i_str+'_'+colcol)
     i+=1
 
-
-# In[54]:
-
-
 ddd_list = {"HITS_SEQ_25":[],"HITS_SEQ_75":[],"SESS_SEQ_25":[],"SESS_SEQ_75":[],"TOT_PAG_VIEW_CT_25":[],"TOT_PAG_VIEW_CT_75":[],"TOT_SESS_HR_V_25":[],"TOT_SESS_HR_V_75":[]}
 for col in col_list:
     data = ddddata_need[ddddata_need['CLAC1_NM']==col]
@@ -432,50 +187,19 @@ for col in col_list:
     ddd_list['TOT_SESS_HR_V_75'].append(data_des.loc['75%']['TOT_SESS_HR_V'])
 
 
-# In[55]:
-
-
 ddd_list_df=pd.DataFrame(ddd_list)
-
-
-# In[56]:
-
-
 ddd_list_df['CLAC1']=col_list
-
-
-# In[150]:
-
-
 ddd_list_df.to_csv("range_session.csv")
-
-
-# In[ ]:
-
-
-
-
-
-# In[57]:
 
 
 data = ddddata_need[ddddata_need['CLAC1_NM']=='가구']
 
-
-# In[58]:
-
-
 data_des=data.describe()
 
 
-# In[59]:
-
+# # Data below 25%
 
 data_des.loc['25%']
-
-
-# In[61]:
-
 
 ddd_list = {"HITS_SEQ_mean":[],"SESS_SEQ_mean":[],"TOT_PAG_VIEW_CT_mean":[],"TOT_SESS_HR_V_mean":[]}
 for col in col_list:
@@ -486,39 +210,8 @@ for col in col_list:
     ddd_list['TOT_PAG_VIEW_CT_mean'].append(data_des.loc['mean']['TOT_PAG_VIEW_CT'])
     ddd_list['TOT_SESS_HR_V_mean'].append(data_des.loc['mean']['TOT_SESS_HR_V'])
 
-
-# In[63]:
-
-
 ddd_list_df = pd.DataFrame(ddd_list)
-
-
-# In[65]:
-
 
 ddd_list_df['CLAC1']=col_list
 
-
-# In[66]:
-
-
-ddd_list_df
-
-
-# In[67]:
-
-
 ddd_list_df.to_csv("logdata_mean.csv")
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
